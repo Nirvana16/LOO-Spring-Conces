88678a2d3e3dc8fcdadafdde758f7161875bfe4d
Nirvana16 <zedanilo161@gmail.com>
2017-11-22 16:10:11 -0200

    vinculado ao MYSQL

--- src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
-insert into veiculo (id, marca, modelo, placa, cor, ano_modelo, motor, combustivel, observacoes) values (1, 'Ford', 'Fusion', 'LKW-8815', 'Verde', '2016/2017', '2.0', 'Gasolina/Alcool', 'nenhuma observacao')
-insert into veiculo (id, marca, modelo, placa, cor, ano_modelo, motor, combustivel, observacoes) values (2, 'VW', 'Golf', 'LKU-4412', 'Branco', '2011/2012', '1.8', 'Gasolina/Alcool', 'Oficina')
-insert into veiculo (id, marca, modelo, placa, cor, ano_modelo, motor, combustivel, observacoes) values (3, 'Cheverolet', 'Celta', 'LPQ-1234', 'Branco', '2010/2011', '1.0', 'Gasolina/Alcool', 'nenhuma observacao')
-insert into veiculo (id, marca, modelo, placa, cor, ano_modelo, motor, combustivel, observacoes) values (4, 'Ford', 'Ka', 'KMU-7358', 'Preto', '2015/2016', '1.5', 'Gasolina/Alcool', 'nenhuma observacao')
-
