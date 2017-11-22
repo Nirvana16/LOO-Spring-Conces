c304666e04ba56200e5f3d2ee8104552ec07a701
Nirvana16 <zedanilo161@gmail.com>
2017-11-22 15:29:52 -0200

    motando combox dinamico

--- src/main/java/com/nirvana/revenda/controller/VeiculosController.java
+++ src/main/java/com/nirvana/revenda/controller/VeiculosController.java
@@ -1,13 +1,17 @@
 package com.nirvana.revenda.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-
+import com.nirvana.revenda.model.Situacao;
 import com.nirvana.revenda.model.Veiculo;
 import com.nirvana.revenda.repository.Veiculos;
 
@@ -25,7 +29,7 @@ public class VeiculosController {
 		mv.addObject("todosVeiculos", veiculos.findAll());
 		mv.addObject(new Veiculo());
 		
-		
+		mv.addObject("todasSituacoes", Situacao.values());	
 
 		
 		return mv;		
@@ -37,10 +41,23 @@ public class VeiculosController {
 		this.veiculos.save(veiculo);
 		ModelAndView mv = new ModelAndView("ListaVeiculos");
 		mv.addObject("mensagem", "Veículo Inserido com Sucesso!!!");
+		
 		mv.addObject("todosVeiculos", veiculos.findAll()); //Para listar os veículos após a inclusão de um novo.
 
+		
 		
 		return mv;
 		
 	}
+	
+	@ModelAttribute("todasSituacoes")
+	public List<Situacao> listaDeSituacoes(){
+		return Arrays.asList(Situacao.values());		
+	}
+	
+//	@ModelAttribute("todosVeiculos")
+//	public List<Veiculo> listaDeVeiculos(){
+//		//List <Convidado> lista = convidados.findAll();
+//		return veiculos.findAll();		
+//	}
 }
